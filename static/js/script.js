3255e05a60794f2f2cd8bab75c568521f84d60ca
aineon <enia.oneill@gmail.com>
2021-05-10 00:24:31 +0000

    Fix issue with flash messages not displaying

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,8 @@
 // Hide flash messages after 2secs
-// setTimeout(()=> {
-//     flash_message = document.getElementsByClassName("flash");
-//     for (let i = 0; i < flash_message.length; i++) {
-//         flash_message[i].style.display="none";
-//     }
-// }, 2000);
+setTimeout(()=> {
+    flash_message = document.getElementsByClassName("flash");
+    for (let i = 0; i < flash_message.length; i++) {
+        flash_message[i].style.display="none";
+    }
+}, 2000);
 
